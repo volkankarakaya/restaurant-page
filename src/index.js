8a160e176c84a325c50ffb6127769ea43165516e
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-03-04 14:25:38 +0300

    configure webpack modules and plug ins

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('hello')
+import img from './background.jpg';
+import './style.css';
+
+const content = document.createElement('div');
+content.setAttribute('id','content' )
+const body = document.querySelector('body');
+body.appendChild(content)
+
+
+const headingElement = document.createElement('h1')
+headingElement.innerHTML = "Welcome to the Acme Restaurant!"
+content.appendChild(headingElement);
+
+const paraElement = document.createElement('p');
+paraElement.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolore consequuntur placeat voluptatem necessitatibus a dolor possimus excepturi ab voluptas?"
+content.appendChild(paraElement)
+
+const imgElement = new Image()
+imgElement.src = img;
+imgElement.setAttribute('width','800')
+
+content.appendChild(imgElement)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output:{
-        filename: 'main.js',
-        path: path.resolve(__dirname,'dist'),
+    entry: {
+        bundel:path.resolve(__dirname,'src/index.js')
     },
+    output:{
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname,'dist'),
+        assetModuleFilename: '[name][ext]',
+        clean:true
+    },
+    devServer:{
+        static:{
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port:3000,
+        open:true,
+        hot:true,
+        compress:true,
+        historyApiFallback: true
+    },
+    devtool:'source-map',
+
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/\.(png|svg|jpg|jpeg|gif)$/,
+                type:'asset/resource',
+            },
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-env']
+                    }
+                },
+            },
+        ],
+    },
+
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: 'Restaurant Page',
+            filename:'index.html',
+        }),
+    ],
 };
